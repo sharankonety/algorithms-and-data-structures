9e6090529ce905c33beb436aabca46b0ae1683cb
sudheer-unify <sudheer@unifytech.com>
2020-10-18 19:38:12 +0530

    ,,,

---
+++ subarraysum.py
@@ -0,0 +1,14 @@
+# sub array sum
+# print the index values of the start and end indices of the array which lead to the given sum
+list = [1,2,3,7,5]
+n = len(list)
+actual_sum = 12
+for i in range(n):
+    current_sum = list[i]
+    for j in range(i+1,n):
+        current_sum += list[j]
+        if current_sum==actual_sum:
+            print(i,j)
+            break
+        if current_sum > actual_sum:
+            break
