3f5f417e43145bcfe6506d4294cca67fc3158fab
sudheer-unify <sudheer@unifytech.com>
2020-10-19 22:14:46 +0530

    ''

--- majorityelement.py
+++ majorityelement.py
@@ -3,7 +3,10 @@ list = [3,1,3,3,2]
 n = len(list)
 num = -1
 for i in list:
-    frequency = list.count(i)
-    if(frequency > n//2):
+    count = 0
+    for j in list:
+        if (i == j):
+            count += 1
+    if(count > n//2):
         num = i
 print(num)
