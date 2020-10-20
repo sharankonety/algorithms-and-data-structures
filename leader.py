e15cd403a2981210c84fa7250fab717e05eec6e6
sudheer-unify <sudheer@unifytech.com>
2020-10-20 22:34:37 +0530

    'leader-program'

---
+++ leader.py
@@ -0,0 +1,11 @@
+list = [16,17,4,3,5,2]
+n = len(list)
+for i in range(n):
+  is_leader = True
+  for j in range(i+1,n):
+    if (list[j] > list[i]):
+      is_leader = False
+      break
+  if is_leader == True :
+    print(list[i]) 
+      
