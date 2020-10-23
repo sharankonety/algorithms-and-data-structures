429bf9350d7188e39cfc793163ff3f943d6211eb
sudheer-unify <sudheer@unifytech.com>
2020-10-23 23:06:22 +0530

    'sorting'

--- kthsmallest.py
+++ kthsmallest.py
@@ -2,6 +2,12 @@
 # testcase 2 : 7 10 4 20 15 k = 4 output = 200
 # where all the elements in the list are distinct
 list = [7,10,4,20,15]
+n = len(list)
 k = 4
-list.sort()
+for i in range(n-1,0,-1):
+    for j in range(i):
+        if (list[j]>list[j+1]):
+            temp = list[j+1]
+            list[j+1] = list[j]
+            list[j] = temp
 print(list[k-1])
